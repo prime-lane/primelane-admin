9f511436b2f5b0bdf074677bac147fbc8f0e53b1
JoshX <joshx.dev@gmail.com>
2025-12-27 15:21:58 +0100

    update summary endpoint & shape

--- src/features/home/types.ts
+++ src/features/home/types.ts
@@ -1,8 +1,14 @@
 export interface DashboardStats {
-    total_rides: number
-    total_cancelled_rides: number
-    cancellation_rate: number
-    total_accepted_rides: number
-    acceptance_rate: number
-    average_rating: number
+    total_trip_revenue: number
+    total_trip_commission: number
+    total_trip_driver_earning: number
+    total_completed_trip_count: number
+    total_one_off_trip_count: number
+    total_hourly_trip_count: number
+    total_customer_count: number
+    total_verified_customer_count: number
+    total_unverified_customer_count: number
+    total_driver_count: number
+    total_verified_driver_count: number
+    total_unverified_driver_count: number
 }
